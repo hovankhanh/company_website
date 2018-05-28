e0261d727d67a6ff0c235f4d56e5d3c384e6c16c
PN\tram.pham <tram.pham@student.passerellesnumeriques.org>
2018-05-28 15:21:13 +0700

    edit frontEnd for Admin, use <router-outlet>

--- sunflower/src/app/app.module.ts
+++ sunflower/src/app/app.module.ts
@@ -1,22 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './admin/header/header.component';
-import { FooterComponent } from './admin/footer/footer.component';
-import { IndexComponent } from './admin/index/index.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './admin/pages/header/header.component';
+import { FooterComponent } from './admin/pages/footer/footer.component';
+import { IndexComponent } from './admin/pages/index/index.component';
+import { ContactComponent } from './admin/pages/contact/contact.component';
+
+const appRoutes: Routes=[
+  
+  {
+    path :'admin',
+    // component: IndexComponent,
+      children:[
+        {
+          path :"contact",
+          component : ContactComponent
+        },
+        {
+          path :"index",
+          component : IndexComponent
+      }
+      ]
+  }  
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    IndexComponent
+    IndexComponent,
+    ContactComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
