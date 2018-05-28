d53aef1d055813412173436fc5571bc32c0414a3
PN\tram.pham <tram.pham@student.passerellesnumeriques.org>
2018-05-28 10:41:38 +0700

    design frontEnd for Admin finished

--- sunflower/src/app/app.module.ts
+++ sunflower/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './admin/header/header.component';
+import { FooterComponent } from './admin/footer/footer.component';
+import { IndexComponent } from './admin/index/index.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule
