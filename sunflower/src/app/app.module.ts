29b11cffd529ba5851d941aabd0b4e77625cf3bf
PN\tram.pham <tram.pham@student.passerellesnumeriques.org>
2018-05-29 10:06:01 +0700

    Design Admin finish everything

--- sunflower/src/app/app.module.ts
+++ sunflower/src/app/app.module.ts
@@ -3,14 +3,45 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { IndexComponent } from './pages/index/index.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes=[
+  {
+    path :'',
+    component: IndexComponent
+  },
+  {
+    path :'admin',
+      children:[
+        {
+          path :"",
+          component : IndexComponent
+        },
+        {
+          path :"contact",
+          component : ContactComponent
+        }
+      ]
+  }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IndexComponent,
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
